52dbfbaa4049283192ce4ad3b53f1456ea19f065
FrancaTM <francatm@outlook.com>
2021-03-24 22:11:34 -0300

    Initial Commit

---
+++ Am I Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Am I Rich
+//
+//  Created by Tulio Marcos Franca on 24/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
